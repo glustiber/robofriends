f1237b865d51555eddba8ff313eae38e71bfc052
Gregory Lustiber <glustiber@gmail.com>
2021-07-07 17:35:45 -0600

    Added AddRobot functionality

--- src/components/AddRobot.js
+++ src/components/AddRobot.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './AddRobot.css';
 
-const AddRobot = ({handleSubmit}) => {
-    return (
-        <div className='addRobotForm'>
-            <p>Add New Robot:</p>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name: </label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="name"
-                    //value={formChange}
-                    //onChange={this.handleFormChange} 
-                    />
-                <label htmlFor="email">Email: </label>
-                <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    placeholder="email"
-                    //value={formChange}
-                    //onChange={this.handleFormChange} 
-                    />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+class AddRobot extends Component {
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            name: '',
+            email: ''
+        };
+        this.state = this.initialState;
+    }
+
+    handleFormChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name] : value
+        });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleSubmit(this.state);
+        this.setState(this.initialState);
+    }
+
+    render() {
+        const { name, email } = this.state;
+
+        return (
+            <div className='addRobotForm'>
+                <p>Add New Robot:</p>
+                <form onSubmit={this.onFormSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="name"
+                        value={name}
+                        onChange={this.handleFormChange} 
+                        />
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="email"
+                        value={email}
+                        onChange={this.handleFormChange} 
+                        />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default AddRobot;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-import AddRobot1 from '../components/AddRobot1';
+import AddRobot from '../components/AddRobot';
 import './App.css';
 
 class App extends Component {
@@ -23,42 +23,18 @@ class App extends Component {
 
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value })
-        //console.log(this.state.formFields.name);
     }
-    
-    /*handleFormChange = (event) => {
-        console.log(event.target);
-    }*/
 
     handleSubmit = newRobot => {
-        console.log(newRobot);
-        console.log(this.state.robots);
         const { robots } = this.state;
-        /*const updatedRobots = robots.push({
-            id: robots.length+1,
-            name: newRobot.name,
-            username: 'test',
-            email: newRobot.email
-        });
-        console.log(updatedRobots);*/
         const robotEntry = [{
-            id: robots.length+1,
+            id: robots[robots.length - 1].id + 1,
             name: newRobot.name,
             username: 'test',
             email: newRobot.email
         }];
-        console.log(robotEntry);
-        //NEED LOGIC TO UPDATE ROBOT LIST WITH NEW ENTRY, MAYBE IN RENDER?
-        //this.setState({robots: [...this.state.robots, robotEntry] })
-        //this.setState({ robots: updatedRobots } );
-        /*this.setState({
-            robots: robots.push({
-                id: robots.length+1,
-                name: newRobot.name,
-                username: 'test',
-                email: newRobot.email
-            })
-        })*/
+
+        this.setState({robots: [...robots, ...robotEntry] })
     }
 
     removeRobot = (key) => {
@@ -77,15 +53,15 @@ class App extends Component {
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        //const { formFields.name, formFields.email } = this.state;
-        if (!robots.length) {           // if (robots.length === 0). 
-            return <h1>Loading</h1>     // Can also make this into a ternary
+        
+        if (!robots.length) {           // if (robots.length === 0). Ternary?
+            return <h1>Loading</h1>
         } else {
             return (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <AddRobot1 handleSubmit={this.handleSubmit}/>
+                    <AddRobot handleSubmit={this.handleSubmit}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={ filteredRobots } removeRobot={this.removeRobot} />
